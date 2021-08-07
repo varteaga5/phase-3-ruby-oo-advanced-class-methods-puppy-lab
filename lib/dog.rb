48c9e41d3dec070f69290ff42149f3cdfb1e116c
vincent <vincent.arteaga@gmail.com>
2021-08-06 20:41:31 -0700

    lab complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+
+class Dog
+    attr_accessor :name, :save
+
+@@all = []  
+
+    def initialize(name)
+        @name = name
+        @save = save
+        
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each{|dog| print dog.name , "\n"}
+    end
+
+    def save
+        @@all << self
+    end
+end
